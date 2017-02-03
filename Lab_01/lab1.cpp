7f494e8754d819f3a18d0e79d3f387e5d1d7eaeb
David <David.Yuan04@myhunter.cuny.edu>
2017-02-03 09:03:39 -0500

    strugglesss

--- Lab_01/lab1.cpp
+++ Lab_01/lab1.cpp
@@ -1,3 +1,10 @@
+/*
+Author: David Yuan
+Course: {135,136}
+Instructor: Mr. Zamansky
+Assignment: Lab 1
+ */
+
 #include <iostream>
 #include <ctime>
 using namespace std;
@@ -12,18 +19,19 @@ int main() {
 	/*cin >> "What hour is it?" >> hour >> endl;
 	cin >> "What minute is it?" >> min >> endl;
 	*/
-   cout << "Enter the hours part of today's sunset time (4-9)" << endl;
+   cout << "Enter the hours part of today's sunset time (4-9)" << endl; //user input
    cin  >> hour;	
-   cout << "Enter the minutes part of today's sunset time (0-59)" << hour << ":" << min << endl;
+   cout << "Enter the minutes part of today's sunset time (0-59)" << endl; //same as above
    cin >> min;
-   if (hour < 12){
-   	hour += 12; 
-   	cout << "Good Morning sir/madam \n" << endl;
+   if (hour < 12){ //if statement, if user input hours < 12
+   	hour += 12; //if user input hours 
+   	cout << "Good Morning sir/madam, the time is \n" << hour << "," << min << endl; //display formatting: hour,min
    }
-   else if (hour >= 12 && hour < 18){
+   else if (hour >= 12 && hour < 18){ //if user input hours after 12 but before 18 (6pm), then afternoon message displays
    	cout << "Good Afternoon sir/madam \n" << endl;
    }
-   else if (hour >= 18 && hour <= 24){
+   else if (hour >= 18 && hour <= 24){ //same as above but for hours 18 to 24 (midnight)
    	cout << "Good Evening sir/madam \n" << endl;
    }
+   cout << "test" << endl;
 }
