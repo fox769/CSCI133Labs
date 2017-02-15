28a2f02cda981889e1f453160ec5ad5936aad862
David <David.Yuan04@myhunter.cuny.edu>
2017-02-15 12:35:44 -0500

    task1complete

--- Lab_02/lab2.cpp
+++ Lab_02/lab2.cpp
@@ -9,18 +9,19 @@ using namespace std;
 
 int main()
 {
-	int p = 10; //starting value, setting all of the parameters
-	float g = 1.25; //growth rate (float val b/c it's a percent)
+	float p = 10; //starting value, setting all of the parameters
+	double g = 1.25; //growth rate (float val b/c it's a percent)
 	int m = 100; //carrying capacity
 	float h = 0.20; //minimum loss rate (float val b/c it's a percent)
 	int k = 20; //number of years passed
-	int y;
-	int z = 0;
+	int z = 1; //the year after year 0, originally had this at 0 but didnt work cuz then year 0 would have pop of 19
 	/*
 	cout << "What is your starting population? (p) \n";
 	cin >> p;
-	cout << "What is your growth rate (as a decimal) ? (g) \n"; 
+	*/
+	cout << "What is your growth rate (as a decimal) ? (g) \n";
 	cin >> g;
+	/*
 	cout << "What is the expected carrying capacity? (m) \n";
 	cin >> m;
 	cout << "What is the minimum loss rate? (as a decimal) (h) \n";
@@ -28,20 +29,15 @@ int main()
 	cout << "How many years do you want to see (k) \n";
 	cin >> k;
 	*/
-	cout << p << endl;
-	cout << g << endl;
-	cout << m << endl;
-	cout << h << endl;
-	cout << k << endl;
 
-	int x = (z+g-h)*p-(p*p/m);
 	cout << "Year    Population" << endl;
-	while (z < k + 1)
+	while (z <= k) //z starts at 1 so loop initiates
 		{
-			cout << z << "	" << (z+g-h)*p-(p*p/m);
-			cout << z << endl;
-			z = z + 1;
+			int y = ((1+g-h)*p-(p*p)/m); //the formula on the page
+			p = y; //p gets updated so that when the loop runs again, the p in the formula will be different from the one before
+			cout << z << "	" << y; //displays the year and population in column format
+			cout << endl; //line break
+			z = z + 1; //z gets updated
 		}
-//		cout << ""
 	return 0;
 }
