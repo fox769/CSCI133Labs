6c3ae9ed27aa842090b736efaf2ab9b72b0dfffd
David <David.Yuan04@myhunter.cuny.edu>
2017-02-15 13:06:35 -0500

    whileloopception

--- Lab_02/lab2.cpp
+++ Lab_02/lab2.cpp
@@ -16,29 +16,34 @@ int main()
 	float h = 0.20; //minimum loss rate (float val b/c it's a percent)
 	int k = 20; //number of years passed
 	int z = 1; //the year after year 0, originally had this at 0 but didnt work cuz then year 0 would have pop of 19
+	int a = 1;
+	while (a <=3 )//we're gonna call the function 3 times
+	{
 	/*
-	cout << "What is your starting population? (p) \n";
-	cin >> p;
+		cout << "What is your starting population? (p) \n";
+		cin >> p;
 	*/
-	cout << "What is your growth rate (as a decimal) ? (g) \n";
-	cin >> g;
+		cout << "What is your growth rate (as a decimal) ? (g) \n";
+		cin >> g;
 	/*
-	cout << "What is the expected carrying capacity? (m) \n";
-	cin >> m;
-	cout << "What is the minimum loss rate? (as a decimal) (h) \n";
-	cin >> h;
-	cout << "How many years do you want to see (k) \n";
-	cin >> k;
-	*/
+		cout << "What is the expected carrying capacity? (m) \n";
+		cin >> m;
+		cout << "What is the minimum loss rate? (as a decimal) (h) \n";
+		cin >> h;
+		cout << "How many years do you want to see (k) \n";
+		cin >> k;
+		*/
 
-	cout << "Year    Population" << endl;
-	while (z <= k) //z starts at 1 so loop initiates
-		{
-			int y = ((1+g-h)*p-(p*p)/m); //the formula on the page
-			p = y; //p gets updated so that when the loop runs again, the p in the formula will be different from the one before
-			cout << z << "	" << y; //displays the year and population in column format
-			cout << endl; //line break
-			z = z + 1; //z gets updated
+		cout << "Year    Population" << endl;
+		while (z <= k) //z starts at 1 so loop initiates
+			{
+				int y = ((1+g-h)*p-(p*p)/m); //the formula on the page
+				p = y; //p gets updated so that when the loop runs again, the p in the formula will be different from the one before
+				cout << z << "	" << y; //displays the year and population in column format
+				cout << endl; //line break
+				z = z + 1; //z gets updated
+			}
+		a = a + 1; //updates a, whileloopception
+		z = 1; //changes z back to 1 so that the while loop in the while loop can perform
 		}
-	return 0;
 }
