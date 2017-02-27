aff3adc6ab7d61b6adf10c4f60cacb4754e3780f
David <David.Yuan04@myhunter.cuny.edu>
2017-02-27 11:02:01 -0500

    incomplete

--- Lab_04/lab4.cpp
+++ Lab_04/lab4.cpp
@@ -1,21 +1,18 @@
+/*   Author: DAVID YUAN   
+Course: {135,136}   
+Instructor: MIKE ZAMANSKY   
+Assignment: LAB 4
+This program takes in numbers (n) and (k) and outputs n!, k!, and (n-k)!.
+*/
+
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
-/*notes
-//double d = 3.3;
-
-//double e = 3.7;
-
-//int j = int(d);
-
-//int j = int(e);
-*/
-
 int factorial(int);
 
-int factorial(int n) 
+int factorial(int n)
 {
 	int num;
     if (n > 1) 
@@ -29,12 +26,13 @@ int factorial(int n)
 }
 
 int c(int n, int k){
-	for(int i=1; i<n; i++)
-	{
-		cout << endl << n * factorial (n-1) << endl << endl;
-		cout << k * factorial (k-1) << endl << endl;
-		cout << (n-k)* factorial (n-k-1) << endl;
-	}
+//	for(int i=1; i<n; i++) //with this for loop it repeats, but w/o it, at the end it outputs 6295680 for some reason
+
+//	{
+		cout << endl << n * factorial (n-1) << endl << endl; //outputs n!
+		cout << k * factorial (k-1) << endl << endl; //outputs k!
+		cout << (n-k)* factorial (n-k-1) << endl; //outputs (n-k)!
+//	}
 }
 
 //int c2(int n, int k);
@@ -42,7 +40,12 @@ int c(int n, int k){
 //double c4(int n, int k);
 
 int main(){
-	cout << endl << c(5,4) << endl << endl;
-
+	cout << endl << c(5,4) << endl << endl; 
+	/*outputs 
+	120 
+	24 
+	1 
+	6295680
+	*/
 	return 0;
 }
