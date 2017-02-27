fcab33fb51af7281b975f7d7933d8ce01c00f77b
David <David.Yuan04@myhunter.cuny.edu>
2017-02-27 10:47:38 -0500

    send_hep

--- Lab_04/lab4.cpp
+++ Lab_04/lab4.cpp
@@ -3,62 +3,46 @@
 using std::cout;
 using std::endl;
 
-int main()
+/*notes
+//double d = 3.3;
+
+//double e = 3.7;
+
+//int j = int(d);
+
+//int j = int(e);
+*/
+
+int factorial(int);
+
+int factorial(int n) 
 {
-  int a,b;
-  double d,e;
+	int num;
+    if (n > 1) 
+    {
+        num = n*factorial(n-1);
+    }
+    else 
+    {
+        return 1;
+    }
+}
 
-  
-  a=9;
-  b=2;
+int c(int n, int k){
+	for(int i=1; i<n; i++)
+	{
+		cout << endl << n * factorial (n-1) << endl << endl;
+		cout << k * factorial (k-1) << endl << endl;
+		cout << (n-k)* factorial (n-k-1) << endl;
+	}
+}
 
-  d=9;
-  e=2;
+//int c2(int n, int k);
 
-  // int / int --> int (truncats, integer division)
-  cout <<  a/b << endl;
+//double c4(int n, int k);
 
-  // double / int --> double (implicit conversion, floating point division)
-  cout <<  d/b << endl;
-  
+int main(){
+	cout << endl << c(5,4) << endl << endl;
 
-  // int / double --> double (implicit conversion) 
-  cout <<  a/e << endl;
-
-  // double / double --> double (no conversion needed)
-  cout <<  d/e << endl;
-
-  // result is truncated
-  b = d/e;
-  cout << b << endl;
-
-  a=9;
-  b=2;
-  d = a/b;
-  // result is truncated because the division is integer
-  cout << d << endl;
-
-
-  a=9;
-  b=2;
-  d = (double)(a/b);
-  // since we do the division before casting due to (),
-  // the result is an int which we then cast.
-  cout << d << endl;
-
-
-  a=9;
-  b=2;
-  d = (double)a/b;
-  // by casting a to double, the result is now double
-  cout << d << endl;
-
-  // The moral of the story is
-  // cast individual variable to int or double:
-  // by doing (int)x or double(x) before you need a certain type
-  // in the expression and make sure to check your results.
-
-  
-
-  return 0;
+	return 0;
 }
